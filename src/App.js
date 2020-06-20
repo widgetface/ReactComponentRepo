7eb4562ed942ebcbc27ad2e0178d1efd6dc1d57a
davet <daveenq@gmail.com>
2020-06-20 15:30:02 +0100

    Refactored file to use sass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+      <div>
         To see the components close the browser and run 
         "npm run storybook"
-      </header>
+      </div>
     </div>
   );
 }
