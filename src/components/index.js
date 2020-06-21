5e0961ac5d511c0ff8f18a76cee1edd71becd258
davet <daveenq@gmail.com>
2020-06-21 16:03:41 +0100

    Added components to index.js for easy import to app

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,14 @@
+//Rating
 export { default as RatingBar } from "../components/rating/RatingBar";
+
+//Blogging
+export { default as AnimePost } from "../components/comments/posts/AminePost";
+
+//Loading
+export { default as AnimeProgress } from "../components/progress/AnimeProgress";
+
+//Menu
+export { default as StyledComponentMenu } from "../components/menu/StyledComponntMenu";
+
+//Cards
+export { default as SpinCard180 } from "../components/cards/SpinCard180";
