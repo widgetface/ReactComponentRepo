16a3c837def3fcaa759ddec175c71c627c28a5a3
davet <daveenq@gmail.com>
2020-06-24 15:12:43 +0100

    Added new example list story and restyled imgs

--- src/stories/list.stories.js
+++ src/stories/list.stories.js
@@ -5,18 +5,23 @@ import { storiesOf } from "@storybook/react";
 import {EdinburghOne, EdinburghTwo, EdinburghThree, EdinburghFour, EdinburghFive, EdinburghSix} from '../assets/imgs'
 storiesOf("Component | Lists/StyledFishEyeList", module)
   .addDecorator(withInfo)
-  .add("StyledFishEyeList with letters", () => {
+  .add("StyledFishEyeList examples", () => {
       const data = [{title: 'A view of Edinburgh', src:EdinburghOne}, {title: 'castle', src:EdinburghTwo}, {title: 'A view of Edinburgh', src:EdinburghThree},{title: 'A view of Edinburgh', src:EdinburghFour},{title: 'A view of Edinburgh', src:EdinburghFive},{title: 'A view of Edinburgh', src:EdinburghSix}];
       const renderFunc = (item, index) => {
           return (< div className="img-list"><img src={item.src} alt={item.title}/></div>)
       }
+      const letters =  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"];
     return (
       <>
-        <div style={{ padding: 10, texAlign: "center", marginTop: 10 }}>
+        <div style={{ padding: 10, texAlign: "center", marginTop: 10, zIndex: 1000 }}>
           <StyledFishEyeList data={data} renderListItemFunc={renderFunc}/>
+        </div>
+        <div style={{ padding: 10, texAlign: "center",  marginTop:50}}>
+          <StyledFishEyeList data={letters} />
         </div>
       </>
     );
   });
+
 
  
